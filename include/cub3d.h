c4a164ec4fc221e7a87ddc2e0f88f31b9f1e41b4
kjroy93 <kmarrero1993@gmail.com>
2026-04-12 20:08:26 +0200

    raycasting for walls done

--- include/cub3d.h
+++ include/cub3d.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kjroydev <kjroydev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/12 19:11:59 by kjroydev          #+#    #+#             */
+/*   Updated: 2026/04/12 19:48:43 by kjroydev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CUB3D_H
 # define CUB3D_H
 
@@ -33,18 +45,63 @@ typedef struct s_image
 	int		endian;
 }	t_image;
 
+/**
+ * @struct s_ray
+ * @brief Structure that contains the information of an `x` ray.
+ * 
+ * This structure contains the representation of a 2D vector `(x, y)`, with the
+ * adition of it's next step. The direction is determinated by the starting
+ * position `(x, y)` of the player in the grid. `direction-x` and `direction-y` 
+ * of the current vector, it's determinated by the `direction-x` and
+ * `direction-y` the player is facing at  the beginning 
+ * of the game/level `(N, S, E, W)`.
+ * 
+ * Composition:
+ * 
+ * - Delta distance the vector can advance from a `(x, y)` starting point
+ * 		without hiting a wall.
+ * 
+ * - Side distance the vector has between the player and
+ * 			a wall/interactive object.
+ * 
+ * - Direction `(x, y)` the vector is facing.
+ * 
+ * - The next step in `(x, y)` it has to advance.
+ * 
+ * - Starting position in `(x, y)`.
+ */
 typedef struct s_ray
 {
+	/** Magnitude the current vector can advance in `x`. */
 	double	delta_dist_x;
+
+	/** Magnitude the current vector can advance in `y`. */
 	double	delta_dist_y;
+
+	/** Distance between vector and player in `x`. */
 	double	side_dist_x;
-	double 	side_dist_y;
+
+	/** Distance between vector and player in `y`. */
+	double	side_dist_y;
+
+	/** Direction of the vector in `x`. */
 	double	dir_x;
+
+	/** Direction of the vector in `y`. */
 	double	dir_y;
-	int		map_x;
-	int		map_y;
+
+	/** Next step in `x`. */
 	int		step_x;
+
+	/** Next step in `y`. */
 	int		step_y;
+
+	/** Max lenght in `x`. */
+	int		map_x;
+
+	/** Max lenght in `y`. */
+	int		map_y;
+	int		side;
 }	t_ray;
 
 typedef struct s_keys
@@ -85,17 +142,66 @@ typedef struct s_player
 	double	plane_y;
 }	t_player;
 
+/**
+ * @struct s_game
+ * @brief Structure that contains all the information of the game.
+ * 
+ * This structure stores relevant information of the game core,
+ * including references to other structures like the
+ * initial configuration information; the keyboard input;
+ * rays of the 2D engine (raycasting) of the game and the player.
+ * 
+ * Composition:
+ * 
+ * - mlx: Pointer to the MiniLibX context (graphics library instance).
+ * 
+ * - win: Pointer to the window created by MiniLibX.
+ * 
+ * - frame: Image buffer used as the current render target.
+ * 
+ * - textures: Array of textures used for wall rendering.
+ * 
+ * - cfg: Parsed configuration data (map, colors, textures paths, etc.).
+ * 
+ * - player: Player state (position, direction, camera plane).
+ * 
+ * - rays: Array of rays casted per screen column (raycasting engine).
+ * 
+ * - keys: Structure storing the current keyboard input state.
+ */
 typedef struct s_game
 {
+	/** Pointer to the MiniLibX */
 	void		*mlx;
+
+	/** Pointer to the windows created by MiniLibX. */
 	void		*win;
+
+	/** Image buffer. */
 	t_image		frame;
+
+	/** Array of textures for wall rendering */
 	t_image		textures[TEX_COUNT];
+
+	/** Parsed configuration. */
 	t_config	cfg;
+
+	/** Player state. */
 	t_player	player;
+
+	/** Array of rays casted per screen column (raycasting engine). */
 	t_ray		rays[WIN_WIDTH];
+
+	/** Keyboard keys functions. */
 	t_keys		keys;
 }	t_game;
+
+// Engine Functions
+
+void	determine_hit(t_game *game, t_map *map, int *x);
+void	precal_camera_factors(double cam_factor[WIN_WIDTH]);
+void	render_frame(t_game *game);
+void	move_player(t_game *game);
 
 int		parse_cub_file(t_config *cfg, const char *path);
 int		parse_header_line(t_config *cfg, const char *line);
@@ -107,8 +213,6 @@ int		key_press(int keycode, t_game *game);
 int		key_release(int keycode, t_game *game);
 int		close_window(t_game *game);
 void	cleanup_game(t_game *game);
-void	render_frame(t_game *game);
-void	move_player(t_game *game);
 int		error_msg(const char *msg);
 int		rgb_to_int(int r, int g, int b);
 int		file_has_extension(const char *file, const char *ext);
--- src/engine/rays.c
+++ src/engine/rays.c
@@ -6,28 +6,98 @@
 /*   By: kjroydev <kjroydev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 20:18:47 by kjroydev          #+#    #+#             */
-/*   Updated: 2026/04/11 00:15:29 by kjroydev         ###   ########.fr       */
+/*   Updated: 2026/04/12 20:04:15 by kjroydev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	precal_camera_factors(double cam_factor[WIN_WIDTH])
+/**
+ * @brief Function to calculate the distance a vector `x` can advance
+ * in `(x, y)` before the colision with other `(x, y)` square in the
+ * the `map->grid`, with the Theorem of Pythagoras.
+ * 
+ * @param game Pointer to the game structure `t_game`, that contains
+ * 				the array of rays `t_rays`.
+ * @param x	Integer that determine the vector `x` from the player position.
+ * 
+ * @note The division between an integer and 0 is protected. The result will be
+ * 		`1e30` in C (infinite). This case ocurrs when the player is facing in
+ * 		a direction where the vector never hits an `x` or `y` side of a square in
+ * 		the grid.
+ */
+static void	pythagoras_tan(t_game *game, int *x)
 {
-	int	x;
+	if (fabs(game->rays[*x].dir_x) < 1e-6)
+		game->rays[*x].delta_dist_x = 1e30;
+	else
+		game->rays[*x].delta_dist_x = fabs(1 / game->rays[*x].dir_x);
+	if (fabs(game->rays[*x].dir_y) < 1e-6)
+		game->rays[*x].delta_dist_y = 1e30;
+	else
+		game->rays[*x].delta_dist_y = fabs(1 / game->rays[*x].dir_y);
+}
 
-	x = 0;
-	while (x < WIN_WIDTH)
+/**
+ * @brief Function that calculates the distance a vector `x` will have
+ * in `(x, y)` from the player, located in a `(x, y)` square in the
+ * the `map->grid`.
+ * 
+ * This determines if the direction of a vector `x` is left (minor than 0)
+ * or right (greater than 0). This includes both `(x, y)`.
+ * 
+ * @param game Pointer to the game structure `t_game`, that contains
+ * 				the array of rays `t_rays`.
+ * @param x	Integer that determine the vector `x` from the player position.
+ * 
+ * @note This function must be used at the beginning of the calculations.
+ * The vectors are normalized, as well as proportionalized. So the `distance`
+ * moving forward will be the same in both axis. The only interrumption will
+ * ocurr when in the loop, the `map_x` and `map_y` are != 0 (a wall).
+ */
+static void	proyect_vector(t_game *game, int *x)
+{
+	if (game->rays[*x].dir_x < 0)
 	{
-		cam_factor[x] = 2.0 * x / (double)(WIN_WIDTH - 1) - 1.0;
-		x++;
+		game->rays[*x].step_x = -1;
+		game->rays[*x].side_dist_x = (game->player.x - game->rays[*x].map_x)
+			* game->rays[*x].delta_dist_x;
+	}
+	else
+	{
+		game->rays[*x].step_x = 1;
+		game->rays[*x].side_dist_x = (game->rays[*x].map_x + 1.0
+				- game->player.x) * game->rays[*x].delta_dist_x;
+	}
+	if (game->rays[*x].dir_y < 0)
+	{
+		game->rays[*x].step_y = -1;
+		game->rays[*x].side_dist_y = (game->player.y
+				- game->rays[*x].map_y) * game->rays[*x].delta_dist_y;
+	}
+	else
+	{
+		game->rays[*x].step_y = 1;
+		game->rays[*x].side_dist_y = (game->rays[*x].map_y + 1.0
+				- game->player.y) * game->rays[*x].delta_dist_y;
 	}
 }
 
-void	create_rays(t_game *game, double cam_factor[WIN_WIDTH])
+/**
+ * @brief Function that cast every single ray from the player perspective.
+ * 
+ * With the reason `y = 2x - 1`, which determines that the `y` is the
+ * wide size of the player FOV, this function calculates the `dir_x`
+ * and `dir_y` of a `x` ray. It also gives each ray, the position
+ * of the player in the `grid` (map).
+ * 
+ * @param game	Pointer to the game structure `t_game`, that contains
+ * 			the array of rays `t_rays`.
+ * @param cam_factor  Wide size of the FOV of the player.
+ * @note the cam_factor cannot be 0, or a allocated space in memory!!
+ */
+static void	cast_rays(t_game *game, double cam_factor[WIN_WIDTH])
 {
-	double	ray_dir_x;
-	double	ray_dir_y;
 	int		x;
 
 	x = 0;
@@ -43,68 +113,41 @@ void	create_rays(t_game *game, double cam_factor[WIN_WIDTH])
 	}
 }
 
-void	hit_calculations(t_game *game)
+/**
+ * @brief Function that begins the raycasting.
+ * 
+ * This determines the perspective betweem the player and the walls. This does
+ * not include objects, doors, enemies or any other objects in the game.
+ * 
+ * @param game Pointer to the game structure `t_game`, that contains
+ * 				the array of rays `t_rays`.
+ * @param map Pointer to the information of the map.
+ * @param x	Integer that determine the vector `x` from the player position.
+ */
+void	look_for_walls(t_game *game, t_map *map, int *x)
 {
-	int		x;
+	double	cam_factor[WIN_WIDTH];
+	int		hit;
 
-	x = 0;
-	if (fabs(game->rays[x].dir_x < 1e-6))
-		game->rays[x].delta_dist_x = 1e30;
-	else
-		game->rays[x].delta_dist_x = fabs(1 / game->rays[x].dir_x);
-	if (fabs(game->rays[x].dir_y < 1e-6))
-		game->rays[x].delta_dist_y = 1e30;
-	else
-		game->rays[x].delta_dist_y = fabs(1 / game->rays[x].dir_y);
-	if (game->rays[x].dir_x < 0)
+	hit = 0;
+	cast_rays(game, cam_factor);
+	pythagoras_tan(game, x);
+	proyect_vector(game, x);
+	while (hit == 0)
 	{
-		game->rays[x].step_x = -1;
-		game->rays[x].side_dist_x = (game->player.x - game->rays[x].map_x)
-			* game->rays[x].delta_dist_x;
+		if (game->rays[*x].side_dist_x < game->rays[*x].side_dist_y)
+		{
+			game->rays[*x].side_dist_x += game->rays[*x].delta_dist_x;
+			game->rays[*x].map_x += game->rays[*x].step_x;
+			game->rays[*x].side = 0;
+		}
+		else
+		{
+			game->rays[*x].side_dist_y += game->rays[*x].delta_dist_y;
+			game->rays[*x].map_y += game->rays[*x].step_y;
+			game->rays[*x].side = 1;
+		}
+		if (map.grid[game->rays[*x].map_y][game->rays[*x].map_x] == '1')
+			hit = 1;
 	}
-	else
-	{
-		game->rays[x].step_x = 1;
-		game->rays[x].side_dist_x = (game->rays[x].map_x + 1.0 - game->player.x)
-			* game->rays[x].delta_dist_x;
-	}
-	if (game->rays[x].dir_y < 0)
-	{
-		game->rays[x].step_y = -1;
-		game->rays[x].side_dist_y = (game->player.y - game->rays[x].map_y)
-			* game->rays[x].delta_dist_y;
-	}
-	else
-	{
-		game->rays[x].step_y = 1;
-		game->rays[x].side_dist_y = (game->rays[x].map_y + 1.0 - game->player.y)
-			* game->rays[x].delta_dist_y;
-	}
-}
-
-void	render_frame(t_game *game)
-{
-	int	x;
-	int	y;
-
-	/*
-	** TODO (renderer): Implement DDA raycasting and textured wall slices.
-	** This skeleton only paints ceiling/floor bands as a placeholder.
-	*/
-	y = 0;
-	while (y < WIN_HEIGHT / 2)
-	{
-		x = 0;
-		while (x < WIN_WIDTH)
-			put_pixel(&game->frame, x++, y, game->cfg.ceil_color);
-		y++;
-	}
-	while (y < WIN_HEIGHT)
-	{
-		x = 0;
-		while (x < WIN_WIDTH)
-			put_pixel(&game->frame, x++, y, game->cfg.floor_color);
-		y++;
-	}
-	mlx_put_image_to_window(game->mlx, game->win, game->frame.img, 0, 0);
 }
